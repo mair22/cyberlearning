3adbe067320fbdc6bc29d9e9dcfcafd8467a929c
mair22 <acediawalker@gmail.com>
2022-06-26 00:46:47 +0700

    testdemo

--- src/App.js
+++ src/App.js
@@ -8,14 +8,11 @@ import ListPage from "./pages/ListPage/ListPage";
 import HomeTemplate from "./templates/HomeTemplate";
 import AdminTemplate from "./templates/AdminTemplate";
 import Info from "./pages/Info/Info";
-<<<<<<< HEAD
 import Demo from "./pages/Demo/Demo";
-=======
 import UserPage from "./pages/UserPage/UserPage";
 import ClassPage from "./pages/ClassPage/ClassPage";
 import UpdateClass from "./pages/ClassPage/UpdateClass";
 import UpdateUser from "./pages/UserPage/UpdateUser";
->>>>>>> master
 
 export const history = createBrowserHistory();
 
@@ -23,13 +20,7 @@ function App() {
   return (
     <Router history={history}>
       <switch>
-<<<<<<< HEAD
-        <HomeTemplate exact path="/loginpage" component={LoginPage} />
-        <HomeTemplate exact path="/registerpage" component={RegisterPage} />
-        <HomeTemplate exact path="/listpage" component={ListPage} />
-        <HomeTemplate exact path="/infopage" component={Info} />
         <HomeTemplate exact path="/demo" component={Demo} />
-=======
         <HomeTemplate exact path="/dangnhap" component={LoginPage} />
         <HomeTemplate exact path="/dangky" component={RegisterPage} />
         <HomeTemplate exact path="/danhmuckhoahoc" component={ListPage} />
@@ -38,7 +29,6 @@ function App() {
         <HomeTemplate exact path="/quanlylophoc" component={ClassPage} />
         <HomeTemplate exact path="/themkhoahoc" component={UpdateClass} />
         <HomeTemplate exact path="/themnguoidung" component={UpdateUser} />
->>>>>>> master
 
 
         <HomeTemplate exact path="/" component={HomePage} />
