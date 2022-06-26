b170160f21778df882a13f94e69443d6a7ecb0c7
mair22 <acediawalker@gmail.com>
2022-06-26 12:11:55 +0700

    api

--- src/pages/HomePage/ListItem.jsx
+++ src/pages/HomePage/ListItem.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "../../assets/scss/pages/_home-page.scss";
+import { http } from '../../ulti/setting';
+// import axios from 'axios'; // no configuration
+import { Button } from "antd";
 
+
+async function dangky(){
+  let result = await http.get('http://elearningnew.cybersoft.edu.vn/api/QuanLyKhoaHoc/LayDanhSachKhoaHoc?MaNhom=GP01');
+  let firstEight = [];
+  try {
+    let data = result.data;
+    // console.log(data);
+    for(let i = 0; i < 8; i++){
+      firstEight.push(data[i]);
+    }
+  }
+  catch(err){
+    console.log(err);
+  }
+
+  for(let data of firstEight){
+    console.log(data);
+  }
+
+}
 export default function ListItem(props) {
   return (
     <section className="listItem">
@@ -13,7 +36,7 @@ export default function ListItem(props) {
               <div className="content">
                 <i className="fab fa-autoprefixer" />
                 <h1>Khóa học</h1>
-                <NavLink className="btn btn-warning" to="/dangky">Đăng ký</NavLink>
+                <button className="btn btn-warning" onClick={dangky}>Đăng ký</button>
               </div>
             </div>
           </div>
