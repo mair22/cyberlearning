32d7e69b4e344efc52d1d8e30ed4363f726647af
mair22 <acediawalker@gmail.com>
2022-07-09 07:18:13 +0700

    layout

--- src/pages/Demo/Demo.jsx
+++ src/pages/Demo/Demo.jsx
@@ -3,71 +3,69 @@
 // import React from 'react';
 // const { Header, Content, Footer, Sider } = Layout;
 
-<<<<<<< HEAD
-export default function 
-() {
-  return (
-    <div>
-      <Layout>
-        <Sider
-          breakpoint="lg"
-          collapsedWidth="0"
-          onBreakpoint={(broken) => {
-            console.log(broken);
-          }}
-          onCollapse={(collapsed, type) => {
-            console.log(collapsed, type);
-          }}
-        >
-          <div className="logo" />
-          <Menu
-            theme="dark"
-            mode="inline"
-            defaultSelectedKeys={['4']}
-            items={[UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
-              (icon, index) => ({
-                key: String(index + 1),
-                icon: React.createElement(icon),
-                label: `nav ${index + 1}`,
-              }),
-            )}
-          />
-        </Sider>
-        <Layout>
-          <Header
-            className="site-layout-sub-header-background"
-            style={{
-              padding: 0,
-            }}
-          />
-          <Content
-            style={{
-              margin: '24px 16px 0',
-            }}
-          >
-            <div
-              className="site-layout-background"
-              style={{
-                padding: 24,
-                minHeight: 360,
-              }}
-            >
-              content
-            </div>
-          </Content>
-          <Footer
-            style={{
-              textAlign: 'center',
-            }}
-          >
-            Ant Design ©2018 Created by Ant UED
-          </Footer>
-        </Layout>
-      </Layout>
-    </div>
-  )
-}
-=======
+// export default function 
+// () {
+//   return (
+//     <div>
+//       <Layout>
+//         <Sider
+//           breakpoint="lg"
+//           collapsedWidth="0"
+//           onBreakpoint={(broken) => {
+//             console.log(broken);
+//           }}
+//           onCollapse={(collapsed, type) => {
+//             console.log(collapsed, type);
+//           }}
+//         >
+//           <div className="logo" />
+//           <Menu
+//             theme="dark"
+//             mode="inline"
+//             defaultSelectedKeys={['4']}
+//             items={[UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
+//               (icon, index) => ({
+//                 key: String(index + 1),
+//                 icon: React.createElement(icon),
+//                 label: `nav ${index + 1}`,
+//               }),
+//             )}
+//           />
+//         </Sider>
+//         <Layout>
+//           <Header
+//             className="site-layout-sub-header-background"
+//             style={{
+//               padding: 0,
+//             }}
+//           />
+//           <Content
+//             style={{
+//               margin: '24px 16px 0',
+//             }}
+//           >
+//             <div
+//               className="site-layout-background"
+//               style={{
+//                 padding: 24,
+//                 minHeight: 360,
+//               }}
+//             >
+//               content
+//             </div>
+//           </Content>
+//           <Footer
+//             style={{
+//               textAlign: 'center',
+//             }}
+//           >
+//             Ant Design ©2018 Created by Ant UED
+//           </Footer>
+//         </Layout>
+//       </Layout>
+//     </div>
+//   )
+// }
 // export default function 
 // () {
 //   return (
@@ -131,7 +129,6 @@ export default function
 //     </div>
 //   )
 // }
->>>>>>> master
 
 
 // const App = () => (
