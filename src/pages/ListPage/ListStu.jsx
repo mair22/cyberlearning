8f6dcf554231417f17b3e25d40a21ba206faef29
Đạt Phạm Thành <thanhdat.12081998@gmail.com>
2022-07-11 14:05:48 +0700

    11/7

--- src/pages/ListPage/ListStu.jsx
+++ src/pages/ListPage/ListStu.jsx
@@ -4,149 +4,176 @@ import { http } from "../../ulti/setting"
 
 
 
-async function dangky1(){
-  let result = await http.get('https://elearningnew.cybersoft.edu.vn/api/QuanLyKhoaHoc/LayKhoaHocTheoDanhMuc?maDanhMuc=TuDuy&MaNhom=GP01');}
+async function dangky1() {
+  let result = await http.get('https://elearningnew.cybersoft.edu.vn/api/QuanLyKhoaHoc/LayKhoaHocTheoDanhMuc?maDanhMuc=TuDuy&MaNhom=GP01');
+}
 
 export default function ListStu(props) {
   return (
-    <div className="container">
-        <div className="">Các khóa học phổ biến</div>
-      <div className="row">
-        <div className="col-3">
-          <div className="item">
+    <div className="listStu">
+      <div className="container">
+        <h2>Các khóa học phổ biến</h2>
+        <div className="list row">
+          <div className="item col-12 col-sm-6 col-lg-3">
             <div className="content">
-              <i className="fab fa-autoprefixer" />
-              <h1>Khóa học</h1>
-              <NavLink className="btn btn-warning" to="/dangky">
-                Đăng ký
-              </NavLink>
+              <div className="thumbnail">
+                <img src="https://elearningnew.cybersoft.edu.vn/hinhanh/342t354y_gp01.png" alt="" />
+              </div>
+              <div>
+                <h1>Thành thạo Bootstrap qua 10 website</h1>
+                <div className="readmore">
+                  <button className="custom-btn animation" >Đăng ký</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="item col-12 col-sm-6 col-lg-3">
+            <div className="content">
+              <div className="thumbnail">
+                <img src="https://elearningnew.cybersoft.edu.vn/hinhanh/342t354y_gp01.png" alt="" />
+              </div>
+              <div>
+                <h1>Thành thạo Bootstrap qua 10 website</h1>
+                <div className="readmore">
+                  <button className="custom-btn animation" >Đăng ký</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="item col-12 col-sm-6 col-lg-3">
+            <div className="content">
+              <div className="thumbnail">
+                <img src="https://elearningnew.cybersoft.edu.vn/hinhanh/342t354y_gp01.png" alt="" />
+              </div>
+              <div>
+                <h1>Thành thạo Bootstrap qua 10 website</h1>
+                <div className="readmore">
+                  <button className="custom-btn animation" >Đăng ký</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="item col-12 col-sm-6 col-lg-3">
+            <div className="content">
+              <div className="thumbnail">
+                <img src="https://elearningnew.cybersoft.edu.vn/hinhanh/342t354y_gp01.png" alt="" />
+              </div>
+              <div>
+                <h1>Thành thạo Bootstrap qua 10 website</h1>
+                <div className="readmore">
+                  <button className="custom-btn animation" >Đăng ký</button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div className="col-3">
-          <div className="item">
-            <div className="content">
-              <i className="fab fa-autoprefixer" />
-              <h1>Khóa học</h1>
-              <NavLink className="btn btn-warning" to="/dangky">
-                Đăng ký
-              </NavLink>
-            </div>
-          </div>
-        </div>
-        <div className="col-3">
-          <div className="item">
-            <div className="content">
-              <i className="fab fa-autoprefixer" />
-              <h1>Khóa học</h1>
-              <NavLink className="btn btn-warning" to="/dangky">
-                Đăng ký
-              </NavLink>
-            </div>
-          </div>
-        </div>
-        <div className="col-3">
-          <div className="item">
-            <div className="content">
-              <i className="fab fa-autoprefixer" />
-              <h1>Khóa học</h1>
-              <NavLink className="btn btn-warning" to="/dangky">
-                Đăng ký
-              </NavLink>
-            </div>
-          </div>
-        </div>
-      </div>
 
-      <div className="row">
-        <div className="col-3">
-          <div className="item">
+        <div className="list row">
+          <div className="item col-12 col-sm-6 col-lg-3">
             <div className="content">
-              <i className="fab fa-autoprefixer" />
-              <h1>Khóa học</h1>
-              <NavLink className="btn btn-warning" to="/dangky">
-                Đăng ký
-              </NavLink>
+              <div className="thumbnail">
+                <img src="https://elearningnew.cybersoft.edu.vn/hinhanh/342t354y_gp01.png" alt="" />
+              </div>
+              <div>
+                <h1>Thành thạo Bootstrap qua 10 website</h1>
+                <div className="readmore">
+                  <button className="custom-btn animation" >Đăng ký</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="item col-12 col-sm-6 col-lg-3">
+            <div className="content">
+              <div className="thumbnail">
+                <img src="https://elearningnew.cybersoft.edu.vn/hinhanh/342t354y_gp01.png" alt="" />
+              </div>
+              <div>
+                <h1>Thành thạo Bootstrap qua 10 website</h1>
+                <div className="readmore">
+                  <button className="custom-btn animation" >Đăng ký</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="item col-12 col-sm-6 col-lg-3">
+            <div className="content">
+              <div className="thumbnail">
+                <img src="https://elearningnew.cybersoft.edu.vn/hinhanh/342t354y_gp01.png" alt="" />
+              </div>
+              <div>
+                <h1>Thành thạo Bootstrap qua 10 website</h1>
+                <div className="readmore">
+                  <button className="custom-btn animation" >Đăng ký</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="item col-12 col-sm-6 col-lg-3">
+            <div className="content">
+              <div className="thumbnail">
+                <img src="https://elearningnew.cybersoft.edu.vn/hinhanh/342t354y_gp01.png" alt="" />
+              </div>
+              <div>
+                <h1>Thành thạo Bootstrap qua 10 website</h1>
+                <div className="readmore">
+                  <button className="custom-btn animation" >Đăng ký</button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div className="col-3">
-          <div className="item">
-            <div className="content">
-              <i className="fab fa-autoprefixer" />
-              <h1>Khóa học</h1>
-              <NavLink className="btn btn-warning" to="/dangky">
-                Đăng ký
-              </NavLink>
-            </div>
-          </div>
-        </div>
-        <div className="col-3">
-          <div className="item">
-            <div className="content">
-              <i className="fab fa-autoprefixer" />
-              <h1>Khóa học</h1>
-              <NavLink className="btn btn-warning" to="/dangky">
-                Đăng ký
-              </NavLink>
-            </div>
-          </div>
-        </div>
-        <div className="col-3">
-          <div className="item">
-            <div className="content">
-              <i className="fab fa-autoprefixer" />
-              <h1>Khóa học</h1>
-              <NavLink className="btn btn-warning" to="/dangky">
-                Đăng ký
-              </NavLink>
-            </div>
-          </div>
-        </div>
-      </div>
 
-      <div className="row">
-        <div className="col-3">
-          <div className="item">
+        <div className="list row">
+          <div className="item col-12 col-sm-6 col-lg-3">
             <div className="content">
-              <i className="fab fa-autoprefixer" />
-              <h1>Khóa học</h1>
-              <NavLink className="btn btn-warning" to="/dangky">
-                Đăng ký
-              </NavLink>
+              <div className="thumbnail">
+                <img src="https://elearningnew.cybersoft.edu.vn/hinhanh/342t354y_gp01.png" alt="" />
+              </div>
+              <div>
+                <h1>Thành thạo Bootstrap qua 10 website</h1>
+                <div className="readmore">
+                  <button className="custom-btn animation" >Đăng ký</button>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="col-3">
-          <div className="item">
+          <div className="item col-12 col-sm-6 col-lg-3">
             <div className="content">
-              <i className="fab fa-autoprefixer" />
-              <h1>Khóa học</h1>
-              <NavLink className="btn btn-warning" to="/dangky">
-                Đăng ký
-              </NavLink>
+              <div className="thumbnail">
+                <img src="https://elearningnew.cybersoft.edu.vn/hinhanh/342t354y_gp01.png" alt="" />
+              </div>
+              <div>
+                <h1>Thành thạo Bootstrap qua 10 website</h1>
+                <div className="readmore">
+                  <button className="custom-btn animation" >Đăng ký</button>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="col-3">
-          <div className="item">
+          <div className="item col-12 col-sm-6 col-lg-3">
             <div className="content">
-              <i className="fab fa-autoprefixer" />
-              <h1>Khóa học</h1>
-              <NavLink className="btn btn-warning" to="/dangky">
-                Đăng ký
-              </NavLink>
+              <div className="thumbnail">
+                <img src="https://elearningnew.cybersoft.edu.vn/hinhanh/342t354y_gp01.png" alt="" />
+              </div>
+              <div>
+                <h1>Thành thạo Bootstrap qua 10 website</h1>
+                <div className="readmore">
+                  <button className="custom-btn animation" >Đăng ký</button>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="col-3">
-          <div className="item">
+          <div className="item col-12 col-sm-6 col-lg-3">
             <div className="content">
-              <i className="fab fa-autoprefixer" />
-              <h1>Khóa học</h1>
-              <NavLink className="btn btn-warning" to="/dangky">
-                Đăng ký
-              </NavLink>
+              <div className="thumbnail">
+                <img src="https://elearningnew.cybersoft.edu.vn/hinhanh/342t354y_gp01.png" alt="" />
+              </div>
+              <div>
+                <h1>Thành thạo Bootstrap qua 10 website</h1>
+                <div className="readmore">
+                  <button className="custom-btn animation" >Đăng ký</button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
